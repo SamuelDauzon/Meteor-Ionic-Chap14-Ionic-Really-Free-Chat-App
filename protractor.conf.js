285f2657280200ec1db4d081b57f7ee49d375899
Dauzon <git@dauzon.com>
2018-03-07 00:10:32 +0100

    Tests : configuration

---
+++ protractor.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--no-sandbox']
+    }
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:8100',
+  
+  framework: 'jasmine',
+  specs: [
+    './e2e/**/*.e2e-spec.ts'
+  ],
+  beforeLaunch: function() {
+    require('ts-node').register({
+      project: 'e2e'
+    });
+  },
+  useAllAngular2AppRoots: true
+};
